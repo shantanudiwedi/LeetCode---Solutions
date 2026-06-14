b092933d215c76465f2d23729687f7c757cd85bf
Shantanu Diwedi <shantanudiwedi2007@gmail.com>
2026-06-14 14:57:05 +0530

    Add LC 349 Intersection of Two Arrays solution

---
+++ Easy/349_Intersection_of_Two_Arrays.py
@@ -0,0 +1,7 @@
+# LC 349 - Intersection of Two Arrays
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1.intersection(set2))
