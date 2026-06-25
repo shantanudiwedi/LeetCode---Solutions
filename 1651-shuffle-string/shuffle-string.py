ffd035fa44d7b11f2243897ad09405b9b54d33b0
Shantanu Diwedi <shantanudiwedi2007@gmail.com>
2026-06-25 22:19:11 +0530

    Delete 1651-shuffle-string directory

--- 1651-shuffle-string/shuffle-string.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def restoreString(self, s, indices):
-        ans = [""] * len(s)
-
-        for i in range(len(s)):
-            ans[indices[i]] = s[i]
-
-        return "".join(ans)
