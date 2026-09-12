264b9fc3eba564bc2d28c8d3a1ee7698120ba914
Shantanu Diwedi <shantanudiwedi2007@gmail.com>
2026-09-12 20:15:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (21.86%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        a = nums[0]
+        b = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            c = max(b, a + nums[i])
+            a = b
+            b = c
+        return b
+        
