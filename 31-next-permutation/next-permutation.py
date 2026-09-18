09cf4cbcb248c99da1375504eb91f156e9e48a1c
Shantanu Diwedi <shantanudiwedi2007@gmail.com>
2026-09-18 11:03:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (21.78%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        n = len(nums)
+
+        # Step 1: Find the first decreasing element from right
+        i = n - 2
+
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        # Step 2: Find element just greater than nums[i]
+        if i >= 0:
+            j = n - 1
+
+            while nums[j] <= nums[i]:
+                j -= 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # Step 3: Reverse the part after i
+        left = i + 1
+        right = n - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
